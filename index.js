ddafb5002d9d0cab65fbbcaea4bef9ec2cd240e6
ZhiLeong <saber_zhileong@hotmail.com>
2022-11-22 22:08:43 -0500

    commit tree 42d79fed24a6e6b291abc1976a4398530756b77d

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+function addingEventListener(){
+const input = document.getElementById('button');
+input.addEventListener('click', 
+function clickAlert(){
+    alert('I was clicked!');
+});
 }
